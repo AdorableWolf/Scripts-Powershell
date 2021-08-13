48b7070cf7356dbded4a13e95b834df8544f80f6
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 12:05:48 +0200

    v.3

--- Scripts-Powershell/Wolf's Symbolic Game Saves/Script Symbolic Gamesave.ps1
+++ Scripts-Powershell/Wolf's Symbolic Game Saves/Script Symbolic Gamesave.ps1
@@ -1,6 +1,8 @@
 ﻿<#
-No tests, won't work at other PC. Tons of errors if location is not existing.
-Games needs to be added to txt file GameList.txt. No spaces allowed before location path.
+Some tests won't work at other PC without small tweaks. 
+If location is not existing, skip.
+Games needs to be added GameList.txt in same location.
+save_container needs to be edited manually inside of the script to path you are using.
 #> 
 $scriptBlock = {
 [System.Collections.ArrayList]$game_pairs = @()
@@ -9,8 +11,14 @@ Set-Location $save_container
 $game_list = Get-Content -Path '.\GameList.txt' 
 foreach($g in $game_list){
 $conv = ConvertFrom-String -Delimiter ';' -PropertyNames Name,Target $g 
-$game_pairs.Add($conv)
 
+if(Test-Path -Path ($conv.'Target')){
+Write-Host 'Target Path Exists:' $conv.'Target'
+$game_pairs.Add($conv) | Out-Null
+}
+else{
+Write-Host 'Target Path Doesn''t Exist:' $conv.'Target'
+}
 }
 <# Use this to select ONE property and index 0
 Write-Host 'Hey! This is following text:'$game_pairs.'Name'[0]
@@ -18,9 +26,16 @@ Write-Host 'Hey! This is following text:'$game_pairs.'Name'[0]
 
 $userinput_a = Read-Host "Enter name of the game."
 $userinput_b = Read-Host "Enter savefile location."
- #>
+#>
 foreach($p in $game_pairs){
+if(Test-Path -Path ($save_container + $p.'Name')){
+Write-Host 'Symbolic Pair Existing. Ignoring:' ($save_container + $p.'Name')
+} 
+else{
+Write-Host 'Symbolic Pair Not Existing. Adding:' ($save_container + $p.'Name')
 cmd /c mklink /D ($save_container + $p.'Name') $p.'Target'
 }
+}
+Write-Host -ForegroundColor Cyan 'Task Completed, Enjoy Your New GameSave Location!'
 }
 Start-Process powershell -Verb runAs -ArgumentList "-NoExit", "-Command & {$scriptBlock}"
