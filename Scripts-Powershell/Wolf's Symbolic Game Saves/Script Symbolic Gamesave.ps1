e569ac5211301d18e7c3fca08d28601d18b2f9fa
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 12:06:06 +0200

    v.4

--- Scripts-Powershell/Wolf's Symbolic Game Saves/Script Symbolic Gamesave.ps1
+++ Scripts-Powershell/Wolf's Symbolic Game Saves/Script Symbolic Gamesave.ps1
@@ -1,17 +1,18 @@
-﻿<#
-Some tests won't work at other PC without small tweaks. 
-If location is not existing, skip.
-Games needs to be added GameList.txt in same location.
-save_container needs to be edited manually inside of the script to path you are using.
-#> 
-$scriptBlock = {
+﻿$scriptBlock = {
 [System.Collections.ArrayList]$game_pairs = @()
 Set-Variable -Name save_container -Value 'D:\Symbolic Game Saves FFS\'
 Set-Location $save_container
 $game_list = Get-Content -Path '.\GameList.txt' 
 foreach($g in $game_list){
 $conv = ConvertFrom-String -Delimiter ';' -PropertyNames Name,Target $g 
-
+$replaceUser = $conv.'Target' -Replace '([A-Z]):\\(Users)\\([a-zA-Z_0-9]+)\\(Documents|Appdata)\\([a-zA-Z_0-9]+)', ('$1:\$2\'+[regex]::escape($env:UserName)+'\$4\$5') 
+$conv.'Target' = $replaceUser
+Write-Host 'Holy cow this' $conv.'Target'
+<#
+if($conv.'Target' -Match '[A-Z]:\Users\'){
+ 
+}
+#>
 if(Test-Path -Path ($conv.'Target')){
 Write-Host 'Target Path Exists:' $conv.'Target'
 $game_pairs.Add($conv) | Out-Null
@@ -26,7 +27,7 @@ Write-Host 'Hey! This is following text:'$game_pairs.'Name'[0]
 
 $userinput_a = Read-Host "Enter name of the game."
 $userinput_b = Read-Host "Enter savefile location."
-#>
+
 foreach($p in $game_pairs){
 if(Test-Path -Path ($save_container + $p.'Name')){
 Write-Host 'Symbolic Pair Existing. Ignoring:' ($save_container + $p.'Name')
@@ -35,7 +36,7 @@ else{
 Write-Host 'Symbolic Pair Not Existing. Adding:' ($save_container + $p.'Name')
 cmd /c mklink /D ($save_container + $p.'Name') $p.'Target'
 }
-}
+}#>
 Write-Host -ForegroundColor Cyan 'Task Completed, Enjoy Your New GameSave Location!'
 }
 Start-Process powershell -Verb runAs -ArgumentList "-NoExit", "-Command & {$scriptBlock}"
