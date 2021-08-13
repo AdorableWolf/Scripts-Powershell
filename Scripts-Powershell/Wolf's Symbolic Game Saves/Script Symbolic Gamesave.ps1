73011030580c76e8aa628c58c010517462ad4b5d
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 11:54:46 +0200

    v.2

--- Scripts-Powershell/Wolf's Symbolic Game Saves/Script Symbolic Gamesave.ps1
+++ Scripts-Powershell/Wolf's Symbolic Game Saves/Script Symbolic Gamesave.ps1
@@ -1,26 +1,26 @@
 ﻿<#
 No tests, won't work at other PC. Tons of errors if location is not existing.
-Games needs to be added in the script itself.
+Games needs to be added to txt file GameList.txt. No spaces allowed before location path.
 #> 
 $scriptBlock = {
+[System.Collections.ArrayList]$game_pairs = @()
 Set-Variable -Name save_container -Value 'D:\Symbolic Game Saves FFS\'
+Set-Location $save_container
+$game_list = Get-Content -Path '.\GameList.txt' 
+foreach($g in $game_list){
+$conv = ConvertFrom-String -Delimiter ';' -PropertyNames Name,Target $g 
+$game_pairs.Add($conv)
 
-[System.Collections.ArrayList]$pairs = @(
-[pscustomobject]@{
-Name='Minecraft Save MultiMC'
-Target='C:\Minecraft\MultiMC'
-},
-[pscustomobject]@{
-Name='7DaysToDie'
-Target='C:\Users\AdorableWolf\AppData\Roaming\7DaysToDie'
 }
-)
-<#
+<# Use this to select ONE property and index 0
+Write-Host 'Hey! This is following text:'$game_pairs.'Name'[0]
+
+
 $userinput_a = Read-Host "Enter name of the game."
 $userinput_b = Read-Host "Enter savefile location."
-#> 
-foreach($p in $pairs){
-cmd /c mklink /D ($save_container + $p.Name) $p.Target
+ #>
+foreach($p in $game_pairs){
+cmd /c mklink /D ($save_container + $p.'Name') $p.'Target'
 }
 }
 Start-Process powershell -Verb runAs -ArgumentList "-NoExit", "-Command & {$scriptBlock}"
